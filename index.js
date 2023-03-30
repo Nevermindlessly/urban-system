531ca460ee2fe173e5fb5552a338a9d7ec906332
jorsh <ssb.nevermind@gmail.com>
2023-03-29 20:41:18 -0400

    Added function to initialize app

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-inquirer
-  .prompt([
     {
       type: 'input',
       name: 'title',
@@ -52,14 +50,17 @@ inquirer
       name: 'email',
       message: 'What is your email address?',
     },
-  ])
-];
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
